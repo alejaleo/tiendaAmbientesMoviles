f0ba66f1c667c76b1c1a03a30dcef323a71b3cf6
alejaleo <aleja-060911@hotmail.com>
2026-05-22 13:58:51 -0500

    eliminacion funcion

--- src/app/data/services/productService.ts
+++ src/app/data/services/productService.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../interfaces/product';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -28,9 +27,5 @@ export class ProductService {
 
   deleteProduct(productId: number | string) {
     this.localProducts = this.localProducts.filter(p => p.id !== productId);
-  }
-
-  addProductToApi(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.url, product);
   }
 }
